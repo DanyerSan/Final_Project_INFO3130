dd6ebc5cfbab7d4b0a7713dbd8688500bc01ecee
DanyerSan <danielvillegas60@gmail.com>
2022-12-05 23:57:43 -0500

    Winter changes

--- src/app/winter/winter.component.ts
+++ src/app/winter/winter.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
 import CabinJson from './winter.json';
- /*export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
-}*/
+
 export interface Cabin{
   Location: string,
   Price: number,
   StarRating:number,
-  CabinId:string,
   Image: string
 }
 
@@ -21,15 +15,6 @@ export interface Cabin{
 })
 
 export class WinterComponent {
-
- /*tiles: Tile[] = [
-    {text: 'One', cols: 2, rows:5, color: 'lightblue'},
-    {text: 'Two', cols: 2, rows: 5, color: 'lightgreen'},
-    {text: 'Three', cols: 2, rows: 5, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 5, color: '#DDBDF1'},
-    {text: 'Five', cols: 2, rows: 5, color: '#DDBDF1'},
-    {text: 'Five', cols: 2, rows: 5, color: '#DDBDF1'},
-  ];*/
 
   cabinsJson: any = CabinJson;
   cabins = <Cabin[]>this.cabinsJson;
